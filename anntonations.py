2700ca9438e65c73f07bb71301f0ec6618afbcf8
Asmita Ojha <asmitaojha05@gmail.com>
2024-02-05 21:18:17 +0545

    frustrated

---
+++ anntonations.py
@@ -0,0 +1,19 @@
+import typing
+# Define a variable of type str
+z: str = "Hello, world!"
+# Define a variable of type int
+x: int = 10
+# Define a variable of type float
+y: float = 1.23
+# Define a variable of type list
+list_of_numbers: typing.List[int] = [1, 2, 3]
+# Define a variable of type tuple
+tuple_of_numbers: typing.Tuple[int, int, int] = (1, 2, 3)
+# Define a variable of type dict
+dictionary: typing.Dict[str, int] = {"key1": 1, "key2": 2}
+# Define a variable of type set
+set_of_numbers: typing.Set[int] = {1, 2, 3}
+name : str = "Asmita's"
+mine : str = "Aary Captain"
+print(name + mine)
+print(name , mine)
