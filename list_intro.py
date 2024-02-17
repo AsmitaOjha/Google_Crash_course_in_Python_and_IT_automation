4369d4d7284a939eb6d8e4def415b47aef562f59
Asmita Ojha <asmitaojha05@gmail.com>
2024-02-17 12:00:18 +0545

    List started

---
+++ list_intro.py
@@ -0,0 +1,10 @@
+siblings = ['Prakash','Aastha','Asmita','Kapil']
+print('jayanti' in siblings)
+print('Prakash' in siblings)
+print(type(siblings))
+print(len(siblings))
+print(siblings[3])
+cousins = ['Puja','Sambhu','Annu','Kabir','Assu','Aastha','Kapil','sani']
+print(cousins[3:])
+print(cousins[:5])
+
